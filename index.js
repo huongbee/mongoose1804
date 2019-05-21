4096e51b136a9c64e900d4b91bcee5357333224c
Huong <huongnguyenak96@gmail.com>
2019-05-21 21:07:36 +0700

    create

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 require('./lib/dbconnect.lib');
 const UserModel = require('./models/user.model');
+
+// insert 1
+// UserModel.create({
+//     email: 'admin02@gmail.com',
+//     name: 'Admin 02'
+// }).then(user=>console.log(user))
+// .catch(err=>console.log(err.message))
+
+// insert more
