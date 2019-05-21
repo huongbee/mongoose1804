5cf0b6bb2f3cbb243e986fa18f75d07880f63a99
Huong <huongnguyenak96@gmail.com>
2019-05-21 21:30:02 +0700

    update

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ const UserModel = require('./models/user.model');
 // .catch(err=>console.log(err.message))
 
 // insert more
+// UserModel.insertMany([
+//     {
+//         email: 'admin03@gmail.com',
+//         name: 'Admin 03',
+//         gender: 'male'
+//     },
+//     {
+//         email: 'guest@gmail.com',
+//         name: 'guest 02',
+//         gender: 'female'
+//     }
+// ]).then(users=>console.log(users))
+// .catch(err=>console.log(err.message))
+
+let _id = '5ce4067ae1417b2aa6c17111'
+UserModel.findOneAndUpdate(
+    { _id },
+    {
+        gender: 'female',
+        password: '333333'
+    },
+    { new: true }
+).then(user=>{
+    if(!user){
+
+    }
+})
+.catch(err=>console.log({error: err.message}))
+
