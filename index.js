102574fd1d1252feffab1f612849545e69c27f00
Huong <huongnguyenak96@gmail.com>
2019-05-23 20:13:33 +0700

    and - or

--- index.js
+++ index.js
@@ -23,18 +23,60 @@ const UserModel = require('./models/user.model');
 // ]).then(users=>console.log(users))
 // .catch(err=>console.log(err.message))
 
-let _id = '5ce4067ae1417b2aa6c17111'
-UserModel.findOneAndUpdate(
-    { _id },
-    {
-        gender: 'female',
-        password: '333333'
-    },
-    { new: true }
-).then(user=>{
-    if(!user){
+// let _id = '5ce4067ae1417b2aa6c17111'
+// UserModel.findOneAndUpdate(
+//     { _id },
+//     {
+//         gender: 'female',
+//         password: '333333'
+//     },
+//     { new: true }
+// ).then(user=>{
+//     if(!user){
 
+//     }
+// })
+// .catch(err=>console.log({error: err.message}))
+
+//delete
+// DELETE FROM users
+// WHERE email = 'guest@gmail.com' 
+// UserModel.findOneAndDelete({ email: 'guest@gmail.com' })
+// .then(result=>console.log(result))
+// .catch(err=>console.log({error: err.message}))
+
+// select
+// SELECT * FROM users
+// WHERE email = 'admin02@gmail.com'  //2
+// OR email = 'admin03@gmail.com' // 3
+// AND id = '5ce4067ae1417b2aa6c17118' // 3
+
+// UserModel.find(
+//     { 
+//         $or: [
+//            { email: 'admin02@gmail.com' },
+//            { email: 'admin03@gmail.com' },
+//            { _id: '5ce404b349003a2a18a8c2ea' }
+//         ]
+//     }
+// )
+// .then(users=>console.log(users))
+// .catch(err=>console.log({error: err.message}))
+
+// SELECT * FROM users
+// WHERE email = 'admin02@gmail.com'  //2
+// OR email = 'admin03@gmail.com' // 3
+// AND id = '5ce4067ae1417b2aa6c17118' // 3
+UserModel.find(
+    { 
+        $or: [
+           { email: 'admin02@gmail.com' },
+           { email: 'admin03@gmail.com' },
+        ],
+        $and: [
+            { _id: '5ce4067ae1417b2aa6c17118' }
+        ]
     }
-})
+)
+.then(users=>console.log(users))
 .catch(err=>console.log({error: err.message}))
-
